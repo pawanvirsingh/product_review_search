f3ac0676591b8dd193019fe4547a3f6676e1328c
pawanvirsingh <pawan.singh@timesinternet.in>
2020-06-07 18:11:36 +0530

    managemnt command for loading foods review data

--- product_review_search/review_management/models.py
+++ product_review_search/review_management/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 class Review(models.Model):
     "for storing review datasets "
-    product_id =  models.CharField(max_length=20)
-    user_id = models.CharField(max_length=20)
+    product_id =  models.CharField(max_length=30)
+    user_id = models.CharField(max_length=30)
     profile_name = models.CharField(max_length=50)
     helpfulness = models.CharField(max_length=50)
     score = models.FloatField()
-    time = models.DateTimeField()
+    time = models.CharField(max_length=30)
     summary = models.CharField(max_length=255)
     text = models.TextField()
     helpfulness_score = models.FloatField()
