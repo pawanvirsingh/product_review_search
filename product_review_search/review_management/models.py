349e9b28a7a9f0fb67560cda666ff672f57e7a94
pawanvirsingh <pawan.singh@timesinternet.in>
2020-06-08 23:23:58 +0530

    adding index on ordering fields

--- product_review_search/review_management/models.py
+++ product_review_search/review_management/models.py
@@ -7,12 +7,13 @@ class Review(models.Model):
     user_id = models.CharField(max_length=30)
     profile_name = models.CharField(max_length=50)
     helpfulness = models.CharField(max_length=50)
-    score = models.FloatField()
+    score = models.FloatField(db_index=True)
     time = models.CharField(max_length=30)
     summary = models.CharField(max_length=255)
     text = models.TextField()
-    helpfulness_score = models.FloatField()
+    helpfulness_score = models.FloatField(db_index=True)
 
     def __str__(self):
         return f"user : {self.user_id} product : {self.product_id}, summary : {self.summary}"
+
 
